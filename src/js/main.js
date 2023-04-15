e90ce52294e9ac08a72a3881570282a29bf87aed
dianerdiana <dianerdiana.de@gmail.com>
2023-04-15 18:35:42 +0700

    Modif path

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
-const main = () => {}
+const main = () => {
+  const inputSearch = $('input[name="search-location"]')
+  const searchContent = $('.dropdown-content')
+
+  inputSearch.on('input', function () {
+    if ($(this).val().length >= 3) {
+      searchContent.removeClass('d-none').addClass('d-flex')
+    } else {
+      searchContent.addClass('d-none')
+    }
+  })
+}
 
 export default main
